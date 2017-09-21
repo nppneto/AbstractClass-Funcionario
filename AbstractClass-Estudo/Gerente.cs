025e1251b36320bee2b81f3061a231a7090966d6
Nelson Pereira Pinto Neto <neelsoonzd@hotmail.com>
2017-09-21 17:07:14 -0300

    Estudo de classes abstratas - Funcionários.

---
+++ AbstractClass-Estudo/Gerente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_Estudo
+{
+    class Gerente : Funcionario
+    {
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+        public override void ImprimeDados()
+        {
+            Console.WriteLine("DADOS DOS FUNCIONÁRIOS");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Código: " + this.Codigo);
+        }
+    }
+}
---
+++ AbstractClass-Estudo/TestaFuncionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_Estudo
+{
+    class TestaFuncionario
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Gerente();
+            f.Codigo = 123;
+
+            f.ImprimeDados();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
